bfab0979c46d7843aa22816e28cadfeaf9bc1f76
Alexander de Clavis <declavis.company@gmail.com>
2016-11-04 12:53:46 +0100

    Adding Ticket Model
    Related Work Items: #7

---
+++ TM/TM/Ticket.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TM
+{
+    enum state { Todo, InProgress, Closed }
+
+    class Ticket
+    {
+        string name;
+        string description;
+        state mystate;
+        string assign;
+        DateTime created;
+        DateTime changed;
+
+        public Ticket(string _name, string _des, state _my, string _ass)
+        {
+            created = changed = DateTime.Now;
+            name = _name;
+            description = _des;
+            mystate = _my;
+            assign = _ass;
+        }
+
+    }
+}
